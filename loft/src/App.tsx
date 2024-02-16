634eec7d6339812f65cfb1ac64e982f5639127be
uvinnobusu <uvinnobusu@gmail.com>
2024-02-16 16:30:03 +0000

    upload files

---
+++ loft/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class App extends React.Component<{}, {}> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {};
+  }
+  render(): JSX.Element {
+    return(
+      <React.StrictMode>
+
+      </React.StrictMode>  
+    );
+  }
+}
+export default App;
---
+++ loft/src/components/IconText/IconText.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class IconText extends React.Component<{}, {}> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {};
+  }
+  render(): JSX.Element {
+    return(
+      <div className="icon-text">
+        
+      </div>
+    );
+  }
+}
+export default IconText;
