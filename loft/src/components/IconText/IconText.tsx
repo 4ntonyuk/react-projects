9524d3476b229a8b32ae9cd4ea507f659917a2c4
uvinnobusu <uvinnobusu@gmail.com>
2024-02-16 18:04:01 +0000

    add sass build

--- loft/src/components/IconText/IconText.tsx
+++ loft/src/components/IconText/IconText.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 
-class IconText extends React.Component<{}, {}> {
-  constructor(props: {}) {
+type TypeProps = {
+  fontSize: string,
+  imgUrl: string,
+  link: string,
+  text: string,
+}
+
+type TypeStatic = {
+
+}
+
+class IconText extends React.Component<TypeProps, TypeStatic> {
+  constructor(props: TypeProps) {
     super(props);
     this.state = {};
   }
   render(): JSX.Element {
     return(
       <div className="icon-text">
-        
+        <img src={this.props.imgUrl} alt="" />
+        {(this.props.link != undefined) ? 
+          <a href={this.props.link} style={{fontSize: this.props.fontSize}}>{this.props.text}</a> 
+          : <span style={{fontSize: this.props.fontSize}}>{this.props.text}</span>} 
       </div>
     );
   }
--- loft/src/index.tsx
+++ loft/src/index.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./css/font.css";
+import "./css/style.css";
 
 ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
