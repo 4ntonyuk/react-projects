b69985a3aa73c9fb4919eaf49a121ee161ca04da
uvinnobusu <uvinnobusu@gmail.com>
2024-02-17 13:12:15 +0000

    change directory structure

--- loft/src/App.tsx
+++ loft/src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import NavPanel from "./components/NavPanel/NavPanel";
-import Header from "./components/Header/Header";
-import RangeOfFurniture from "./components/RangeOfFurniture/RangeOfFurniture";
+import IconText from "./components/IconText/IconText";
 
 class App extends React.Component<{}, {}> {
   constructor(props: {}) {
@@ -11,9 +9,45 @@ class App extends React.Component<{}, {}> {
   render(): JSX.Element {
     return(
       <React.StrictMode>
-        <NavPanel />
-        <Header />
-        <RangeOfFurniture />
+        <div className="nav-panel">
+          <div className="nav-panel__wrapper container">
+            <div className="nav-panel__menu">
+              <a href="#!">Главная</a>
+              <a href="#!">О нас</a>
+              <a href="#!">Контакты</a>
+            </div>
+            <div className="nav-panel__delivery">
+              <IconText imgUrl="./img/nav-panel/phone.svg" fontSize="14px" 
+                        text="8 (964) 89 99 119" color="#fff" />
+              <IconText imgUrl="./img/nav-panel/delivery-icon.svg" link="#!" fontSize="14px" 
+                        text="Доставка" color="#fff"/>
+            </div>
+          </div>
+        </div>
+        <div className="header container">
+          <a href="#!" className="header__logo">
+            <img src="./img/header/logo.svg" />
+          </a>
+          <div className="header__search">
+            <img src="./img/header/search-icon.svg" className="search-icon" />
+            <input type="search" placeholder="Поиск" />
+          </div>
+          <div className="header__buttons">
+            <a href="#!"><img src="./img/header/wishlist-icon.svg" /></a>
+            <a href="#!"><img src="./img/header/bag.svg" /></a>
+            <a href="#!"><img src="./img/header/profile-icon.svg" /></a>
+          </div>
+        </div>
+        <div className="range-of-furniture container">
+          <IconText imgUrl="./img/range-of-furniture/kitchen-icon.svg" link="#!" fontSize="16px" text="Кухни"/>
+          <IconText imgUrl="./img/range-of-furniture/bedroom-icon.svg" link="#!" fontSize="16px" text="Спальни"/>
+          <IconText imgUrl="./img/range-of-furniture/livingroom-icon.svg" link="#!" fontSize="16px" text="Гостинные"/>
+          <IconText imgUrl="./img/range-of-furniture/closet-icon.svg" link="#!" fontSize="16px" text="Прихожие"/>
+          <IconText imgUrl="./img/range-of-furniture/office-icon.svg" link="#!" fontSize="16px" text="Офисная мебель"/>
+          <IconText imgUrl="./img/range-of-furniture/childrensroom-icon.svg" link="#!" fontSize="16px" text="Детская"/>
+          <a href="#!" className="font-contrast-color">Акции</a>
+          <button className="etc-button"><img src="./img/range-of-furniture/etc.svg" /></button>
+        </div>
       </React.StrictMode>  
     );
   }
